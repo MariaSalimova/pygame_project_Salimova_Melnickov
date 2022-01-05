79ee84eb1101c919ee77704c96b8a6ca5c6ab90b
Maria <mashasalimova777@gmail.com>
2022-01-05 23:36:04 +0500

    добавила текстуры врага, плитки, карты 1 и 2 уровней

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 # Значения
 FPS = 60
 GRAVITY = 1
-TILE_SIZE = 250
+TILE_SIZE = 500
 
 # Пути
 PATH_OF_TEXTURE = 'data/texture'
@@ -16,3 +16,10 @@ PATH_OF_MC_RUN_LEFT1 = 'texture/entities/main_character/main_character_run/mc_ru
 PATH_OF_MC_RUN_LEFT2 = 'texture/entities/main_character/main_character_run/mc_run_left2.png'
 PATH_OF_MC_RUN_RIGHT1 = 'texture/entities/main_character/main_character_run/mc_run_right1.png'
 PATH_OF_MC_RUN_RIGHT2 = 'texture/entities/main_character/main_character_run/mc_run_right2.png'
+
+PATH_OF_ENEMY_RUN_LEFT1 = 'texture/entities/enemy/enemy_run/enemy_run1l.png'
+PATH_OF_ENEMY_RUN_RIGHT1 = 'texture/entities/enemy/enemy_run/enemy_run1r.png'
+PATH_OF_ENEMY_RUN_LEFT2 = 'texture/entities/enemy/enemy_run/enemy_run2l.png'
+PATH_OF_ENEMY_RUN_RIGHT2 = 'texture/entities/enemy/enemy_run/enemy_run2r.png'
+PATH_OF_ENEMY_IDLE_LEFT = 'texture/entities/enemy/enemy_idle/enemy_idle_l.png'
+PATH_OF_ENEMY_IDLE_RIGHT = 'texture/entities/enemy/enemy_idle/enemy_idle_r.png'
